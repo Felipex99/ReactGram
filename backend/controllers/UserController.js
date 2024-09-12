6e2c0b652fe0a43e277b7f682b5b78a44581cd15
felipenunes <felipenuneto@gmail.com>
2024-09-12 14:10:47 -0300

    UserController

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,9 +7,42 @@ const jwtSecret = process.env.JWT_SECRET
 const generateToken = (id)=>{
     return jwt.sign({id}, jwtSecret,{expiresIn:"7d"})
 }
+
 //Registra usuário e sign in
-const register = async (req, res)=>{
-    res.send("API DE REGISTRO DE USUÁRIO")
+// const register = async (req, res)=>{
+//     res.send("API DE REGISTRO DE USUÁRIO")
+// }
+
+const register = async(req,res)=>{
+    const {name, email, password, confirmPassword} = req.body
+    //Checar se o usuário já existe
+    const user = await User.findOne({email})
+    if(user){
+        res.status(422).json({errors:["Por favor utilizar outro email"]})
+    }
+    //GERAR O HASH PRO PASSWORD
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password,salt)
+
+    //create user
+    const newUser = await User.create({
+        name,
+        email,
+        password:passwordHash
+    })
+
+    //se usuario for criado retorne o token
+    if(!newUser){
+        res.status(422).json(
+            {errors:["Houve um erro por favor tente mais tarde"],}
+        )
+        return
+    }
+
+    res.status(201).json({
+        id: newUser.id,
+        token: generateToken(newUser._id)
+    })
 }
 
 module.exports = {register,}
