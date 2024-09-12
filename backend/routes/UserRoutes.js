77c8db18b886cf0c88cff143f1ce21f9df435754
felipenunes <felipenuneto@gmail.com>
2024-09-12 09:34:21 -0300

    Validações do usuário foram atualizadas

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,7 +2,11 @@ const express = require("express")
 const router = express.Router()
 //controller
 const { register } = require("../controllers/UserController")
+//Middlewares
+const validate = require("../middleware/handleValidation")
+const { userCreateValidation } = require("../middleware/userValidation")
 //ROUTES
-router.post("/register",register)
+router.post("/register", userCreateValidation(), validate, register)
+
 
 module.exports = router
