aca9c73070a3226bc0db03a73323b59b4aea2519
Felipe Nunes <felipenuneto@gmail.com>
2024-09-12 04:28:53 -0300

    Criando o UserController

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,15 @@
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const jwtSecret = process.env.JWT_SECRET
+//Gerar o token do usuario
+const generateToken = (id)=>{
+    return jwt.sign({id}, jwtSecret,{expiresIn:"7d"})
+}
+//Registra usuário e sign in
+const register = async (req, res)=>{
+    res.send("API DE REGISTRO DE USUÁRIO")
+}
+
+module.exports = {register}
