632155eddd86c7e1bf118c5063681279ca92543e
Felipe Nunes <felipenuneto@gmail.com>
2024-09-12 04:40:51 -0300

    Adicionando a rota do UserControllers na Router.js

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express()
 router.get("/", (req,res)=>{
-    res.send("API working, sending response")
+    res.send("API de rotas working, sending response")
 })
 
+router.get("/api/users/register")
 module.exports=router
